7f65c77bea99900f50c13e4e62fb9f54f033b161
orlandogit <orlandogutierrezlermanda@gmail.com>
2019-08-13 00:15:12 -0400

    test de aplicativo

---
+++ src/main/java/com/aqua/AplicativoUnoApplication.java
@@ -0,0 +1,13 @@
+package com.aqua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicativoUnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicativoUnoApplication.class, args);
+	}
+
+}
